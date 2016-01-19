e2b3401b07d3d1f9e9c1a11bec0b6a5995d46070
master404 <tpnve@mail.ru>
2016-01-19 22:51:30 +0200

    Залил проект

---
+++ Client/Client/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public class UdpFileClient
+{
+    // Детали файла
+    [Serializable]
+    public class FileDetails
+    {
+        public string FILETYPE = "";
+        public long FILESIZE = 0;
+    }
+
+    private static FileDetails fileDet;
+
+    // Поля, связанные с UdpClient
+    private static int localPort = 5002;
+    private static UdpClient receivingUdpClient = new UdpClient(localPort);
+    private static IPEndPoint RemoteIpEndPoint = null;
+
+    private static FileStream fs;
+    private static Byte[] receiveBytes = new Byte[0];
+    private static string returnData="";
+    [STAThread]
+    static void Main(string[] args)
+    {
+            Thread tRec = new Thread(new ThreadStart(ReceiverText));
+            tRec.Start();
+//            String[] elements = Regex.Split(returnData.ToString(), ",");
+    }
+    private static void GetFileDetails()
+    {
+        try
+        {
+            Console.WriteLine("-----------*******Ожидание информации о файле*******-----------");
+
+            // Получаем информацию о файле
+            receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+            Console.WriteLine("----Информация о файле получена!");
+
+            XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
+            MemoryStream stream1 = new MemoryStream();
+
+            // Считываем информацию о файле
+            stream1.Write(receiveBytes, 0, receiveBytes.Length);
+            stream1.Position = 0;
+
+            // Вызываем метод Deserialize
+            fileDet = (FileDetails)fileSerializer.Deserialize(stream1);
+            Console.WriteLine("Получен файл типа ." + fileDet.FILETYPE +
+                " имеющий размер " + fileDet.FILESIZE.ToString() + " байт");
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+    }
+
+    private static void GetFileDetails2(byte[] receiveBytes)
+    {
+        try
+        {
+
+            Console.WriteLine("----Информация о файле получена!");
+
+            XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
+            MemoryStream stream1 = new MemoryStream();
+
+            // Считываем информацию о файле
+            stream1.Write(receiveBytes, 0, receiveBytes.Length);
+            stream1.Position = 0;
+
+            // Вызываем метод Deserialize
+             fileDet = (FileDetails)fileSerializer.Deserialize(stream1);
+             Console.WriteLine("Получен файл типа ." + fileDet.FILETYPE +
+                 " имеющий размер " + fileDet.FILESIZE.ToString() + " байт");
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+    }
+    public static void ReceiveFile()
+    {
+        try
+        {
+            Console.WriteLine("-----------*******Ожидайте получение файла*******-----------");
+
+            // Получаем файл
+            receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            // Преобразуем и отображаем данные
+            Console.WriteLine("----Файл получен...Сохраняем...");
+
+            // Создаем временный файл с полученным расширением
+            fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fs.Write(receiveBytes, 0, receiveBytes.Length);
+
+            Console.WriteLine("----Файл сохранен...");
+
+            Console.WriteLine("-------Открытие файла------");
+
+            // Открываем файл связанной с ним программой
+            Process.Start(fs.Name);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+            fs.Close();
+            receivingUdpClient.Close();
+            Console.Read();
+        }
+    }
+
+    public static void ReceiveFile2(byte[] receiveBytes)
+    {
+        try
+        {
+            Console.WriteLine("-----------*******Ожидайте получение файла*******-----------");
+
+            Console.WriteLine("----Получено " + receiveBytes.Length + " Байт...Сохраняем...");
+
+            Console.WriteLine("----Сохранено...");
+
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+            //fs.Close();
+            //receivingUdpClient.Close();
+            //Console.Read();
+        }
+    }
+
+    public static void ReceiveBigFile(Byte[] receiveBytes)
+    {
+        try
+        {
+            Console.WriteLine("-----------*******Ожидайте получение файла*******-----------");
+
+            // Получаем файл
+           // receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            // Преобразуем и отображаем данные
+            Console.WriteLine("----Файл получен...Сохраняем...");
+
+            // Создаем временный файл с полученным расширением
+            fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fs.Write(receiveBytes, 0, receiveBytes.Length);
+
+            Console.WriteLine("----Файл сохранен...");
+
+            Console.WriteLine("-------Открытие файла------");
+
+            // Открываем файл связанной с ним программой
+            Process.Start(fs.Name);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+          //  fs.Close();
+            receivingUdpClient.Close();
+            Console.Read();
+        }
+    }
+    static Byte[] bytesm= new Byte[0];
+    static bool Big = false;
+    static int nak=0;
+    public static void ReceiverText()
+    {
+        // Создаем UdpClient для чтения входящих данных
+        // UdpClient receivingUdpClient2 = new UdpClient(localPort);
+        // IPEndPoint RemoteIpEndPoint2 = null;
+
+        try
+        {
+            Console.WriteLine(
+               "\n-----------*******Общий чат*******-----------");
+
+            while (true)
+            {
+                // Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    returnData = Encoding.UTF8.GetString(receiveBytes);
+                    if (returnData.ToString() == "Small")
+                    {
+                        GetFileDetails();
+                        ReceiveFile();
+                        returnData = "";
+                    }
+                    else if (returnData.ToString().Substring(0, 4) == "Info")
+                    { GetFileDetails(); }
+                    else if (returnData.ToString().Substring(0, 3) == "Big" )
+                    {
+                        if (Big == false) {
+                            String[] elements = Regex.Split(returnData.ToString(), ",");
+                            Console.WriteLine(" --> " + elements[0] + " " + elements[1]);
+                            nak = Convert.ToInt32(elements[1]);
+                            Big = true;
+                            returnData = "";
+                        }
+                    }
+                    else if(Big == true)
+                    {
+                        bytesm = bytesm.Concat(receiveBytes).ToArray();
+                        ReceiveFile2(receiveBytes);
+                        nak = nak - 1;
+                        Console.WriteLine(" --> " + nak);
+                        if (nak <= 0)
+                        {
+                            Console.WriteLine(" ---End--- " );
+                            Big = false;
+                            fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                            fs.Write(bytesm, 0, bytesm.Length);
+                            Process.Start(fs.Name);
+                        }
+                        returnData = "";
+                       // Console.WriteLine(" --> " + returnData.ToString());
+                    }
+                }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+        }
+    }
+
+}
---
+++ Server/ClientServer/Program.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.Threading;
+
+public class UdpFileServer
+{
+    // Информация о файле (требуется для получателя)
+    [Serializable]
+    public class FileDetails
+    {
+        public string FILETYPE = "";
+        public long FILESIZE = 0;
+    }
+
+    private static FileDetails fileDet = new FileDetails();
+
+    // Поля, связанные с UdpClient
+    private static IPAddress remoteIPAddress;
+    private const int remotePort = 5002;
+    private static UdpClient sender = new UdpClient();
+    private static IPEndPoint endPoint;
+
+    // Filestream object
+    private static FileStream fs;
+    [STAThread]
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Получаем удаленный IP-адрес и создаем IPEndPoint
+           // Console.WriteLine("Введите удаленный IP-адрес");
+           // remoteIPAddress = IPAddress.Parse(Console.ReadLine().ToString());//"127.0.0.1");
+            remoteIPAddress = IPAddress.Parse("127.0.0.1");
+            endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+            // Получаем путь файла и его размер (должен быть меньше 8kb)
+            Console.WriteLine("Введите путь к файлу и его имя");
+            fs = new FileStream(@Console.ReadLine().ToString(), FileMode.Open, FileAccess.Read);
+            
+            
+            if (fs.Length > 8192)
+            {
+                Console.WriteLine("Файл больше 8кБ");
+                SendText("Info");
+                SendFileInfo();
+                Thread.Sleep(2000);
+                SendText("Big," + (fs.Length / 8100+1));
+                SendBigFile();
+                fs.Close();
+                return;
+            }
+            else
+            SendText("Small");
+            
+            // Отправляем информацию о файле
+            SendFileInfo();
+
+            // Ждем 2 секунды
+            Thread.Sleep(2000);
+
+            // Отправляем сам файл
+            SendFile();
+
+            Console.ReadLine();
+
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+    }
+    public static void SendFileInfo()
+    {
+
+        // Получаем тип и расширение файла
+        fileDet.FILETYPE = fs.Name.Substring((int)fs.Name.Length - 3, 3);
+
+        // Получаем длину файла
+        fileDet.FILESIZE = fs.Length;
+
+        XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
+        MemoryStream stream = new MemoryStream();
+
+        // Сериализуем объект
+        fileSerializer.Serialize(stream, fileDet);
+
+        // Считываем поток в байты
+        stream.Position = 0;
+        Byte[] bytes = new Byte[stream.Length];
+        stream.Read(bytes, 0, Convert.ToInt32(stream.Length));
+
+        Console.WriteLine("Отправка деталей файла...");
+
+        // Отправляем информацию о файле
+        sender.Send(bytes, bytes.Length, endPoint);
+        stream.Close();
+
+    }
+    private static void SendBigFile()
+    {
+        Byte[] bytes = new Byte[fs.Length];
+        fs.Read(bytes, 0, bytes.Length);
+        int numBytesToRead = 8190;
+        int n = 0;
+        long del = fs.Length / numBytesToRead;
+        for (int i = 0; i <= del; i++)
+        {
+            if (i == del) { numBytesToRead = Convert.ToInt32(fs.Length - numBytesToRead * del); }
+            Byte[] bytes_min = new Byte[numBytesToRead];
+            n = 0;
+            for (int j = i * numBytesToRead; j < i * numBytesToRead + numBytesToRead; j++)
+            {
+                bytes_min[n] = bytes[j];
+                n++;
+            }
+            SendFile2(bytes_min);
+            Thread.Sleep(500);
+        }
+    }
+    private static void SendFile()
+    {
+        // Создаем файловый поток и переводим его в байты
+        Byte[] bytes = new Byte[fs.Length];
+        fs.Read(bytes, 0, bytes.Length);
+
+        Console.WriteLine("Отправка файла размером " + fs.Length + " байт");
+        try
+        {
+            // Отправляем файл
+            sender.Send(bytes, bytes.Length, endPoint);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        finally
+        {
+            // Закрываем соединение и очищаем поток
+            fs.Close();
+            sender.Close();
+        }
+        Console.WriteLine("Файл успешно отправлен.");
+        Console.Read();
+    }
+
+    private static void SendFile2(Byte[] bytes)
+    {
+        Console.WriteLine("Отправка части файла размером " + bytes.Length + " байт");
+        try
+        {
+            // Отправляем файл
+            sender.Send(bytes, bytes.Length, endPoint);
+        }
+        catch (Exception eR)
+        {
+            Console.WriteLine(eR.ToString());
+        }
+        Console.WriteLine("Успешно отправлено.");
+    }
+
+    private static void SendText(string datagram)
+    {
+        // Создаем UdpClient
+        UdpClient sender2 = new UdpClient();
+
+        // Создаем endPoint по информации об удаленном хосте
+        IPEndPoint endPoint2 = new IPEndPoint(remoteIPAddress, remotePort);
+
+        try
+        {
+            // Преобразуем данные в массив байтов
+            byte[] bytes = Encoding.UTF8.GetBytes(datagram);
+
+            // Отправляем данные
+            sender2.Send(bytes, bytes.Length, endPoint2);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+        }
+        finally
+        {
+            // Закрыть соединение
+            sender2.Close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+/*
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UdpSample
+{
+    class Chat
+    {
+        private static IPAddress remoteIPAddress;
+        private static int remotePort;
+        private static int localPort;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Получаем данные, необходимые для соединения
+                Console.WriteLine("Укажите локальный порт");
+                localPort = Convert.ToInt16(Console.ReadLine());
+                //localPort = 5001;
+                //localPort = 5002;
+                Console.WriteLine("Укажите удаленный порт");
+                remotePort = Convert.ToInt16(Console.ReadLine());
+                //remotePort = 5002;
+                //remotePort = 5001;
+                //Console.WriteLine("Укажите удаленный IP-адрес");
+                //remoteIPAddress = IPAddress.Parse(Console.ReadLine());
+                remoteIPAddress=IPAddress.Parse("127.0.0.1");
+                //remoteIPAddress = IPAddress.Parse("192.168.0.64");
+               
+                //////////////////Определяю IP/////////////////
+                //String host = System.Net.Dns.GetHostName();
+                //System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
+                //Console.WriteLine(ip);
+                ///////////////////////////////////////////////
+                
+                // Создаем поток для прослушивания
+                Thread tRec = new Thread(new ThreadStart(Receiver));
+                tRec.Start();
+
+                while (true)
+                {
+                    Send(Console.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+        }
+
+        private static void Send(string datagram)
+        {
+            // Создаем UdpClient
+            UdpClient sender = new UdpClient();
+
+            // Создаем endPoint по информации об удаленном хосте
+            IPEndPoint endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+            try
+            {
+                // Преобразуем данные в массив байтов
+                byte[] bytes = Encoding.UTF8.GetBytes(datagram);
+
+                if (datagram == "start")
+                {
+                    bytes = File.ReadAllBytes("123.png");
+                    Console.WriteLine("Поехали...");
+                }
+
+                // Отправляем данные
+                sender.Send(bytes, bytes.Length, endPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+            finally
+            {
+                // Закрыть соединение
+                sender.Close();
+            }
+        }
+        static int flag=0;
+        public static void Receiver()
+        {
+            // Создаем UdpClient для чтения входящих данных
+            UdpClient receivingUdpClient = new UdpClient(localPort);
+
+            IPEndPoint RemoteIpEndPoint = null;
+
+            try
+            {
+                Console.WriteLine(
+                   "\n-----------*******Общий чат*******-----------");
+
+                while (true)
+                {
+                    
+                    // Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(
+                       ref RemoteIpEndPoint);
+                    if (flag == 1)
+                    {
+                        Console.WriteLine(" --> Получен файл");
+                        File.WriteAllBytes(@"D:\savedFile.png", receiveBytes);
+                        flag = 0;
+                    }
+                    else
+                    {
+                        // Преобразуем и отображаем данные
+                        string returnData = Encoding.UTF8.GetString(receiveBytes);
+
+                        if (returnData.ToString() == "start")
+                        {
+                            flag = 1;
+                        }
+
+                        Console.WriteLine(" --> " + returnData.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
+            }
+        }
+    }
+}
+*/
